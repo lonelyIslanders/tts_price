51dae4404b5b0432738902f6fb4e95858c404c97
pengju <nnqaqpengju@163.com>
2022-10-17 23:48:49 +0800

    提交

---
+++ tts.js
@@ -0,0 +1,62 @@
+// Depends on tencentcloud-sdk-nodejs version 4.0.3 or higher
+const tencentcloud = require('tencentcloud-sdk-nodejs-tts');
+const fs = require('fs');
+const request = require('sync-request');
+const sound = require('play-sound')(opts = {});
+// const exec = require('child_process').exec;
+
+const TtsClient = tencentcloud.tts.v20190823.Client;
+
+
+//AKIDQgorfEbmhLh5KNI2au6B9xZ1IROI5hM5
+//dD6uVt3e8f6RipiP2jMpDRTM3f4HJbij
+
+// 实例化一个认证对象，入参需要传入腾讯云账户secretId，secretKey,此处还需注意密钥对的保密
+// 密钥可前往https://console.cloud.tencent.com/cam/capi网站进行获取
+const clientConfig = {
+  credential: {
+    secretId: "AKID73j7E6h14ycz5tQggPr2wZv0nJXVCcle",
+    secretKey: "f3B2bttc5IE0WliOs64mDPKxKQElBKBl",
+  },
+  region: "ap-nanjing",
+  profile: {
+    httpProfile: {
+      endpoint: "tts.tencentcloudapi.com",
+    },
+  },
+};
+
+// 实例化要请求产品的client对象,clientProfile是可选的
+const client = new TtsClient(clientConfig);
+const reqTime = Date.now().toString();
+
+const priceBuffer = request(`GET`, `http://price.spbad.asia`);
+const price = JSON.parse(priceBuffer.body);
+const BTC = price[0].price;
+const ETH = price[1].price;
+const nowTime = new Date().getSeconds();
+const lastData = { TIME: nowTime, BTC: parseFloat(BTC), ETH: parseFloat(ETH) };
+fs.writeFileSync('lastPrice.json', JSON.stringify(lastData));
+
+const params = {
+  "Text": `比特币价格为${parseFloat(BTC)}，以太坊价格为${parseFloat(ETH)}`,
+  "SessionId": reqTime,
+  "VoiceType": 1007
+};
+client.TextToVoice(params).then(
+  (data) => {
+    // console.log(data);
+    fs.writeFileSync('priceBase64.txt', data.Audio);
+    const au = Buffer.from(data.Audio.toString(), 'base64');
+    fs.writeFileSync('price.wav', au);
+    sound.play('price.wav', (err) => {
+      if (err) {
+        console.log(err)
+      }
+    })
+
+  },
+  (err) => {
+    console.error("error", err);
+  }
+);
